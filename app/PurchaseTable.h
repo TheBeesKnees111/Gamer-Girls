bf1306f8feb43c4788ab9323445a14afca4fc10a
Weston Mathews <westonmathews@gmail.com>
2020-11-30 14:41:15 -0800

    PurchaseTable.h finished

--- app/PurchaseTable.h
+++ app/PurchaseTable.h
@@ -14,58 +14,85 @@ namespace Ui {
 class PurchaseTable;
 }
 
+/*!
+ * @class PurchaseTable
+ * @brief The PurchaseTable class contains all objects necessary to manage customer purchases
+ */
 class PurchaseTable : public QDialog
 {
     Q_OBJECT
 
 public:
-    // Col Count
+    /// Column Count
     const int TABLE_COL_COUNT = 6;
-    // Col Positions
+    /// Column Positions
     enum tableColPositions { KEY, STADIUM, TEAM, S_NAME, S_PRICE, QTY };
-    // Header names
+    /// Column Header names
     QStringList tableHeaders { "KEY", "Stadium", "Team Name", "Souvenir Name", "Souvenir Price", "Quantity" };
-    // Spinbox minimum
+    /// Souvenir Spinbox minimum
     const int SPIN_MIN = 0;
-    // Spinbox maximum
+    /// Souvenir Spinbox maximum
     const int SPIN_MAX = 419;
-    // Vector of pointers to spinbox pointers. Needed for dynamically creating spinboxes on purchase page
+    /// Vector of pointers to spinbox pointers. Needed for dynamically creating spinboxes on purchase page
     QVector<QSpinBox*>* spinBoxes;
 
-    // Initialize Purchase Table
+    /*!
+     * @brief Initializes Purchase Table to blank
+     */
     void InitializePurchaseTable();
 
-    // Populate Purchase Table
+    /*!
+     * @brief Populates Purchase Table with necessary data
+     */
     void PopulatePurchaseTable();
 
-    // Insert dynamic spinboxes
+    /*!
+     * @brief Inserts dynamic spinboxes into rightmost column of purchase table
+     */
     void InsertSpinBoxCol();
 
-    // Clear table
+    /*!
+     * @brief Helper function deletes all table rows to initialize table to blank
+     */
     void DeleteAllTableRows();
 
-    // Destroy team object for future use
+    /*!
+     * @brief Destroys shopping cart so that it can be repopulated in future purchases
+     */
     void DestroyTeams();
 
-    // Collect quantity of each souvenir purchased and add to team object
+    /*!
+     * @brief Collects quantity of each souvenir that customer wishes to purchase for use in creating receipt and record
+     */
     void GetSouvenirQtys();
 
-    // Imports Team data from any trip. Populates purchase table
+    /*!
+     * @brief Parameterized construct takes in list of visited stadium to populate shopping cart
+     * @param *parent; Pointer to parent object
+     * @param *teamList; Pointer to trip list used to populate shopping cart
+     */
     explicit PurchaseTable(QWidget *parent, QVector<Team*>* teamList);
 
     ~PurchaseTable();
 
 public slots:
+    /*!
+     * @brief Slot to take action when quantity of spinboxes is changed
+     * @param value; New value selected by user
+     */
     void qtyChanged(int value);
 
 private slots:
 
+    /*!
+     * @brief Finalizes purchase in record and writes to database
+     */
     void on_okay_pushButton_clicked();
 
 private:
-    Ui::PurchaseTable *ui;
-    QVector<Team*>* teamCart;
-    Database* db;
+    Ui::PurchaseTable *ui; /// Pointer to ui object
+    QVector<Team*>* teamCart; /// Pointer to shopping list information
+    Database* db; /// Pointer to database object
 };
 
 #endif // PurchaseTable_H
