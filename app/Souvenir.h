309e36c2fbed100ed961f28f033305b7659432c7
Weston Mathews <westonmathews@gmail.com>
2020-11-30 14:47:55 -0800

    Souvenir.h finished

--- app/Souvenir.h
+++ app/Souvenir.h
@@ -4,37 +4,119 @@
 #include <QObject>
 #include <QVector>
 
+/*!
+ * @class Souvenir
+ * @brief The Souvenir class represents souvenirs available for purchase at stadiums
+ */
 class Souvenir : public QObject
 {
     Q_OBJECT
 
-    int     souvenirID;
-    int     teamID;
-    QString itemName;
-    float   itemPrice;
-    int     qtyPurchased;
+    int     souvenirID; /// Souvenir ID
+    int     teamID; /// Team ID
+    QString itemName; /// Souvenir name
+    float   itemPrice; /// Souvenir price
+    int     qtyPurchased; /// Quantity of souvenir purchased
 
 public:
+    /*!
+     * @class Constructor
+     * @param *parent; Pointer to parent object
+     */
     explicit Souvenir(QObject *parent = nullptr);
 
+    /*!
+     * @brief Accessor returns souvenir's ID
+     * @return int; Souvenir's ID
+     */
     int     getSouvenirID() const;
+
+    /*!
+     * @brief Accessor returns team's ID
+     * @return int; Team's ID
+     */
     int     getTeamID() const;
+
+    /*!
+     * @brief Accessor returns souvenir's name
+     * @return QString; Souvenir's name
+     */
     QString getItemName() const;
+
+    /*!
+     * @brief Accessor returns souvenir's price
+     * @return float; Souvenir's price
+     */
     float   getItemPrice() const;
+
+    /*!
+     * @brief Accessor returns quantity of souvenir purchased
+     * @return int; Quantity of souvenir purchased
+     */
     int     getQtyPurchased() const;
 
 public slots:
+
+    /*!
+     * @brief Mutator alters souvenir's ID
+     * @param value; Souvenir's ID
+     */
     void setSouvenirID(int value);
+
+    /*!
+     * @brief Mutator alters team's ID
+     * @param value; team's ID
+     */
     void setTeamID(int value);
+
+    /*!
+     * @brief Mutator alters souvenir's name
+     * @param value; Souvenir's name
+     */
     void setItemName(const QString &value);
+
+    /*!
+     * @brief Mutator alters souvenir's price
+     * @param value; Souvenir's price
+     */
     void setItemPrice(float value);
+
+    /*!
+     * @brief Mutator alters quantity of souvenir purchased
+     * @param value; Quantity of souvenir purchased
+     */
     void setQtyPurchased(int value);
 
 signals:
+
+    /*!
+     * @brief Takes action when souvenir ID is changed
+     * @param newID; Souvenir's new ID
+     */
     void souvenirIDChanged(int newID);
+
+    /*!
+     * @brief Takes action when souvenir's team's ID is changed
+     * @param newID; Souvenir's team's new ID
+     */
     void teamIDChanged(int newID);
+
+    /*!
+     * @brief Takes action when souvenir name is changed
+     * @param newID; Souvenir's new name
+     */
     void itemNameChanged(QString newID);
+
+    /*!
+     * @brief Takes action when souvenir price is changed
+     * @param num; Souvenir's new price
+     */
     void itemPriceChanged(float num);
+
+    /*!
+     * @brief Takes action when quantity of souvenirs purchased is changed
+     * @param value; Quantity of souvenirs purchased
+     */
     void qtyPurchasedChanged(int value);
 };
 
